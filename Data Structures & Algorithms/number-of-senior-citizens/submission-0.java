0894d7f3279232a5927e9cfe282b48025786f672
sudhanshuj48 <jsudhanshu48@gmail.com>
2026-08-08 12:38:52 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count =0 ;
+        for(int i=0 ; i< details.length ; i++){
+            if(details[i].charAt(11) > '6'){
+                  count++;      
+            }
+            else if(details[i].charAt(11) == '6'){
+                if(details[i].charAt(12) > '0'){
+                    count++;
+                }
+            }
+        }
+        return count ;
+    }
+}
